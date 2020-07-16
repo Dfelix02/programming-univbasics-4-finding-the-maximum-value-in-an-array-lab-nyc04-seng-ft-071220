7d3d5f462ee959e684feec146b11cf2dff75c258
David Felix <david.felix02@hotmail.com>
2020-07-16 18:31:23 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,8 +2,8 @@ def find_max_value(array)
   max = array[0]
   length = array.size
   length.times do |index|
-    if max < array[index+1]
-      max = array[index+1]
+    if max < array[index]
+      max = array[index]
     end
   end
   max
